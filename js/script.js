37052622e2a2ddacbd0413a4c69dc2488520bfdf
Matia <matia.casu@gmail.com>
2021-04-09 19:06:01 +0200

    Aggiunta QueryString ai parametri di chiamata axios

--- js/script.js
+++ js/script.js
@@ -6,14 +6,17 @@ const app = new Vue ({
   },
   methods: {
     searchMovie: function(){
-      axios.get("https://api.themoviedb.org/3/search/movie?api_key=6e64cd787a186dfc16b5296cbc3e06c2&language=it-IT",{
+      axios.get("https://api.themoviedb.org/3/search/movie",{
         params: {
+          api_key: "6e64cd787a186dfc16b5296cbc3e06c2",
+          language: "it-IT",
           query: this.inputSearch
         }
       })
       .then( response =>{
         this.chosenMovies = response.data.results;
         console.log(response.data.results);
+        this.inputSearch = "";
       });
     }
   }
